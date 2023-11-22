e034c8fa50571b85cd96f6f2adfafc2cd13aa19e
Sivakumar, Mithusan <mithusan.sivakumar@zuehlke.com>
2023-11-22 17:51:14 +0100

    stop edit employee, if show salary is clicked

--- backend/src/employees/repository/employee.repository.ts
+++ backend/src/employees/repository/employee.repository.ts
@@ -5,18 +5,39 @@ import { Injectable } from '@nestjs/common';
 export class EmployeeRepository {
   employees: Employee[] = [
     {
-      id: '0fbf492d-820b-48a4-ab5a-fa3aae822434',
-      firstName: 'Peter',
-      lastName: 'Muster',
-      role: 'Developer',
-      salary: 100000,
+      id: '5787b3bd-04e1-45cd-9d5d-8be1bf7dcad0',
+      firstName: 'Veronique',
+      lastName: 'Gronaver',
+      role: 'Project Manager',
+      salary: 82208,
     },
     {
-      id: '0fdf492d-820b-48a4-ab5a-fa3aae822434',
-      firstName: 'Peter',
-      lastName: 'Muster',
-      role: 'Developer',
-      salary: 100000,
+      id: '462145e6-1862-422f-9eaa-381792f345e5',
+      firstName: 'Silva',
+      lastName: 'Rodell',
+      role: 'Surveyor',
+      salary: 93040,
+    },
+    {
+      id: 'e35717a7-de4a-4a43-86d6-e915be667363',
+      firstName: 'Jody',
+      lastName: 'Riccardi',
+      role: 'Construction Expeditor',
+      salary: 138292,
+    },
+    {
+      id: 'bbd7d160-d1c7-4eb9-a3bf-06e6361d5540',
+      firstName: 'Eileen',
+      lastName: 'Vink',
+      role: 'Construction Foreman',
+      salary: 104879,
+    },
+    {
+      id: '7e3373f6-5504-4f9c-b3a8-c7f002e3158b',
+      firstName: 'Brigitte',
+      lastName: 'Rosenberg',
+      role: 'Construction Worker',
+      salary: 136940,
     },
   ];
 
